13c1a96790f87de3ede5d38c937cfcdc3d0cb8b6
Suyel Bishnu <s2301242@BCVFVFLU1WG7Air.home>
2024-04-06 16:36:47 +0300

    Add header

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,9 +1,13 @@
 import{FC} from 'react';
+import logo from './assets/react.svg';
 interface Props{}
 const Header: FC<Props> = () => {
   return (
-    <div>Header</div>
-    )
+    <div className='py-3 px-2' style={{borderBottom:'1px solid #white'}}>
+        <img src={logo}alt="logo" style={{height:'40px', verticalAlign:'top'}} />
+        <span className='h2 pt-4 m-2 text-white'>React Counter</span>
+    </div>
+    );
 };
 
 export default Header;

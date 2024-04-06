0e372b0043303f8601850afefacc33acdecae5fc
Suyel Bishnu <s2301242@BCVFVFLU1WG7Air.home>
2024-04-06 16:47:25 +0300

    Add buttons and click event

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,9 +1,17 @@
-
-
 const Counter = () => {
+const handleAttack = () =>{
+    alert('Attack Clicked:')
+};
+const handleDefence = () =>{
+    alert('Defend Clicked:')
+};
   return (
-    <div>Counter</div>
-  )
-}
+    <div className="row text-white">
+        <h1>Counter:</h1>
+        <button onClick={handleAttack} style={{width:'200px'}}>+1</button>
+        <button onClick={handleDefence} style={{width:'200px'}}>-1</button>
+    </div>
+  );
+};
 
 export default Counter
